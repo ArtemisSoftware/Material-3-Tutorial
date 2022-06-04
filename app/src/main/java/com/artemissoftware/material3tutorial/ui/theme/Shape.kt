16b1176f92ebec88d75857f5bdc419c5f7d07816
gustavo <gustavo.isaac@gmail.com>
2022-06-04 20:51:49 +0100

    Shapes update

--- app/src/main/java/com/artemissoftware/material3tutorial/ui/theme/Shape.kt
+++ app/src/main/java/com/artemissoftware/material3tutorial/ui/theme/Shape.kt
@@ -1,11 +1,11 @@
 package com.artemissoftware.material3tutorial.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Shapes
 
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    large = RoundedCornerShape(20.dp)
 )
